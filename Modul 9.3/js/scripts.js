1733ec170f558b8a28b635fa7e20dfd66f5f7f19
Michał Gacek <gacek.michael@outlook.com>
2017-02-12 19:20:17 +0100

    fixing

--- Modul 9.3/js/scripts.js
+++ Modul 9.3/js/scripts.js
@@ -2,21 +2,19 @@
  * Created by piotr on 05.02.2017.
  */
 $(function(){
-    //ten kod wykona się po załadowaniu DOM
+    var carouselList = $("#carousel ul");
+
+    setInterval(changeImages, 2000);
+
+    function changeImages() {
+        carouselList.animate({'marginLeft':-400}, 1000, moveFirstSlide);
+    }
+
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+    }
 });
-
-var carouselList = $("#carousel ul");
-
-setInterval(changeImages, 4000);
-
-function changeImages() {
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-}
-
-
-function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginRight: 0});
-}
